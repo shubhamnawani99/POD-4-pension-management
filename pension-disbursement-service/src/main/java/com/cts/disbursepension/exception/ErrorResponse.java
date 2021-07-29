32d6f9845816b47a31d0eac3c950839ed0dfe508
Shubham Nawani <shubhamnawani99@gmail.com>
2021-07-29 11:02:35 +0530

    Revert "Handle service down exception in process-pension"
    This reverts commit 72d70e02511de5196e65ab3726ccf8e991527afe.

--- pension-disbursement-service/src/main/java/com/cts/disbursepension/exception/ErrorResponse.java
+++ pension-disbursement-service/src/main/java/com/cts/disbursepension/exception/ErrorResponse.java
@@ -1,12 +1,7 @@
 package com.cts.disbursepension.exception;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonInclude.Include;
-
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 /**
@@ -16,7 +11,6 @@ import lombok.Getter;
  *
  */
 @Getter
-@AllArgsConstructor
 public class ErrorResponse {
 	private LocalDateTime timestamp;
 	private String message;
@@ -25,11 +19,5 @@ public class ErrorResponse {
 		this.timestamp = LocalDateTime.now();
 		this.message = message;
 	}
-	
-	/**
-	 * Used only for input validation errors
-	 */
-	@JsonInclude(Include.NON_NULL)
-	private List<String> fieldErrors;
 
 }
--- pension-disbursement-service/src/main/java/com/cts/disbursepension/exception/GlobalExceptionHandler.java
+++ pension-disbursement-service/src/main/java/com/cts/disbursepension/exception/GlobalExceptionHandler.java
@@ -3,7 +3,6 @@ package com.cts.disbursepension.exception;
 
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,9 +11,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import feign.FeignException;
 import lombok.extern.slf4j.Slf4j;
@@ -29,9 +25,6 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
-	@Autowired
-	private ObjectMapper objectMapper;
-
 	/**
 	 * This method handles data with invalid arguments
 	 */
@@ -45,30 +38,15 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 	/**
 	 * This will handle all Feign related Exception
 	 * 
-	 * @author Shubham Nawani, Anas Zubair
 	 * @param exception
 	 * @param response
 	 * @return ErrorResponse
 	 */
 	@ExceptionHandler(FeignException.class)
-	public ResponseEntity<ErrorResponse> handleFeignStatusException(FeignException exception,
-			HttpServletResponse response) {
+	public ResponseEntity<String> handleFeignStatusException(FeignException exception, HttpServletResponse response) {
 		log.debug("Handling Feign Client");
-		log.debug("Message: {}", exception.getMessage());
-		ErrorResponse errorResponse;
-		log.debug("UTF-8 Message: {}", exception.contentUTF8());
-		if (exception.contentUTF8().isBlank()) {
-			errorResponse = new ErrorResponse("Invalid Request");
-		} else {
-			try {
-				log.debug("Trying...");
-				errorResponse = objectMapper.readValue(exception.contentUTF8(), ErrorResponse.class);
-			} catch (JsonProcessingException e) {
-				errorResponse = new ErrorResponse(exception.getCause().getMessage());
-				log.error("Processing Error {}", e.toString());
-			}
-		}
-		return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+		log.debug("Message: {}",exception.getMessage());
+		return new ResponseEntity<>(exception.contentUTF8(), HttpStatus.BAD_REQUEST);
 	}
 
 	/**
@@ -82,7 +60,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 	public ResponseEntity<ErrorResponse> handleInvalidTokenException(InvalidTokenException exception,
 			HttpServletResponse response) {
 		log.debug("Handling Invalid Token exception");
-		log.debug("Message: {}", exception.getMessage());
+		log.debug("Message: {}",exception.getMessage());
 		return new ResponseEntity<>(new ErrorResponse(exception.getMessage()), HttpStatus.FORBIDDEN);
 	}
 
